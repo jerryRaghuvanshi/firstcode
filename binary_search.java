2307e670fd320f633cf4c8c3c5ae5aba6dadbcee
jerryRaghuvanshi <59023162+jerryRaghuvanshi@users.noreply.github.com>
2024-06-13 14:03:31 +0530

    new binary search

--- binary_search.java
+++ binary_search.java
@@ -1,7 +1,7 @@
 public class binary_search {
     public static void main(String[] args) {
         int[] arr = {1, 23, 34, 67, 89, 99};
-        int target = 5;
+        int target = 22;
         int ans = bin(arr, target);
         System.out.println(ans);
     }
@@ -9,18 +9,26 @@ public class binary_search {
     static int bin(int[] arr, int target) {
         int start = 0;
         int end = arr.length - 1;
+        if (target > arr[end] || target < arr[start]) {
+            return -1;
+        }
+
         while (start <= end) {
             int mid = start + (end - start) / 2;
+
             if (arr[mid] > target) {
                 end = mid - 1;
-            }
-            if (arr[mid] < target) {
+            } if (arr[mid] < target) {
                 start = mid + 1;
 
             }
             if (arr[mid] == target) {
-                return mid + 1;
+                return mid;
             }
+            if (start-end==1 && arr[start]!=target){
+                return -1;
+            }
+
         }
         return start;
     }
