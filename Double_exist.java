00dbe62bf7b3f8f69552933170142f80e35e4b2c
jerryRaghuvanshi <59023162+jerryRaghuvanshi@users.noreply.github.com>
2024-06-13 15:11:02 +0530

    final

--- Double_exist.java
+++ Double_exist.java
@@ -1,13 +1,32 @@
 public class Double_exist {
     public static void main(String[] args) {
-
+int [] nums ={3,3,7,7,10,11,11};
+int ans =exist(nums);
+        System.out.println(ans);
     }
-    static int exist(int[] arr){
-        int start =0;
-        int end=arr.length;
-        while (start<=end){
-            int mid = start+(end-start)/2;
-            if()
+
+    static int exist(int[] nums) {
+        int start = 0;
+        int end = nums.length-1;
+        if(end==0){
+            return nums[end];
         }
+        while (start <= end) {
+            if (nums[start] == nums[start + 1]) {
+                start += 2;
+            } else {
+                return nums[start];
+            }
+            if (nums[end] == nums[end - 1] ) {
+                end -= 2;
+            } else {
+                return nums[end];
+            }
+
+        }
+        return -1;
+
+
+
     }
 }
